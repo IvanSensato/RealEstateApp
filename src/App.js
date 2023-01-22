46ff75501a089f382f3f230c93b19d4618088c8b
IvanSensato <ivan@sensatolabs.com>
2023-01-22 15:01:21 +0100

    create header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Home from "./Pages/Home"; 
-import Profile from "./Pages/Profile"; 
-import SignIn from "./Pages/SignIn"; 
-import SignUp from "./Pages/SignUp"; 
-import ForgotPassword from "./Pages/ForgotPassword"; 
-import Offers from "./Pages/Offers"; 
+import Home from "./pages/Home"; 
+import Profile from "./pages/Profile"; 
+import SignIn from "./pages/SignIn"; 
+import SignUp from "./pages/SignUp"; 
+import ForgotPassword from "./pages/ForgotPassword"; 
+import Offers from "./pages/Offers"; 
+import Header from "./components/Header";
 function App() {  return (
   
     <> 
       <Router>
+       <Header></Header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
