9b42688f25e0a37493c2966f41e6f04f68980fa9
IvanSensato <ivan@sensatolabs.com>
2023-01-25 15:06:07 +0100

    create private route and custom hooks, protect profile and logout feature

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,5 +1,52 @@
+import { useState } from "react"
+import { getAuth } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 export default function Profile() {
+  const auth  = getAuth()
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+
+  });
+  const {name, email} =formData
+  function onLogout(){
+    auth.signOut()
+    navigate("/")
+  }
   return (
-    <div>Profile</div>
+    <>
+    
+    <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
+      <h1 className="text-3xl text-center text-bold text-white mt-6">My Profile</h1>
+      <div className="w-full md:w-[50%] mt-6 px-3">
+        <form>
+          {/*Name Input */}
+
+          <input type="text" id="name" value={name}
+          disabled className="w-full px-4 py-2 text-xl text-gray-700
+           bg-white border border-gray300 rounded transition ease-in-out mb-6" />
+
+           {/*Name Input */}
+
+           <input type="text" id="email" value={email}
+          disabled className="w-full px-4 py-2 text-xl text-gray-700
+           bg-white border border-gray300 rounded transition ease-in-out mb-6" />
+
+          <div className="flex justify-between text-white whitespace-nowrap
+          text-sm sm:text-lg mb-6">
+            <p className="flex items-center">Do you want to change your name?
+              <span className="text-red-600 hover:text-red-700 transition ease-in-out
+              duration-200 ml-1 cursor-pointer">Edit</span>
+            </p>
+            <p onClick={onLogout} className="text-sensato-blue hover:text-blue-300 transition ease-in-out
+              duration-200 cursor-pointer">Sign Out</p>
+          </div>
+        </form>
+      </div>
+    </section>
+
+    </>
   )
 }
