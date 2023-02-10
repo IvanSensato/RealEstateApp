5a175cc1e16fdad0970f0099fe2e49a7555e83ff
IvanSensato <ivan@sensatolabs.com>
2023-02-10 18:49:02 +0100

    Add delete and edit to listings

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import Moment from 'react-moment'
 import {Link} from 'react-router-dom'
 import {MdLocationOn} from "react-icons/md"
+import {FaTrash} from "react-icons/fa"
+import { MdEdit } from "react-icons/md";
 
-export default function ListingItem({listing, id}) {
+export default function ListingItem({listing, id, onEdit, onDelete}) {
   return (
-  <li className='relative bg-sensato-blue flex flex-col justify-between
-  items-center shadow-md hover:shadow-xl rounded-md overflow-hidden
+  <li className='overflow-hidden relative bg-sensato-blue flex flex-col justify-between
+  items-center shadow-md hover:shadow-xl rounded-md
   transition-shadow duration-150 m-[10px]'>
     <Link to ={`/category/${listing.type}/${id}`}>
-      <img className='h-[170px] w-full object-cover
+      <img className=' w-full object-cover
       hover:scale-105 transition-scale duration-200 ease-in
       '
       loading='lazy' 
@@ -47,7 +49,24 @@ export default function ListingItem({listing, id}) {
         </div>
       </div>
     </Link>
+    {onDelete &&(
+      <FaTrash className='absolute bottom-2 right-2 h-[14px]
+      cursor-pointer text-red-500'
+      onClick={()=>
+        onDelete (listing.id)
+        } 
+       />
+       
+    )}
+        {onEdit &&(
+      <MdEdit className='absolute bottom-2 right-7 h-4
+      cursor-pointer'
+      onClick={()=>
+        onEdit (listing.id)
+        } 
+       />
+       
+    )}
+            
   </li>
-  )
-  
-}
+)}
