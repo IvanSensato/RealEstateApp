7b964f07c19274e5a7c7e8dc27d82727fa6ab7cb
IvanSensato <ivan@sensatolabs.com>
2023-02-13 20:06:05 +0100

    Share link functionality

--- src/Pages/Listing.jsx
+++ src/Pages/Listing.jsx
@@ -6,11 +6,13 @@ import { db } from '../firebase';
 import {Swiper, SwiperSlide} from "swiper/react"
 import SwiperCore, {EffectFade, Autoplay, Navigation,Pagination} from "swiper"
 import "swiper/css/bundle"
+import { FiShare2 } from "react-icons/fi";
 
 export default function Listing() {
     const params = useParams()
     const [listing, setListing] = useState(null)
     const [loading, setLoading] = useState(true)
+    const[shareLinkCopied, setShareLinkCopied] = useState(false)
     SwiperCore.use([
         Autoplay,
         Navigation,
@@ -48,6 +50,23 @@ export default function Listing() {
                 </SwiperSlide>
             ))}
         </Swiper>
+        <div className=' fixed top-[8%] right-[3%] z-10
+        bg-white border-gray-400 rounded-full cursor-pointer border-2 w-12 h-12
+       flex justify-center items-center ' onClick={()=>{
+        navigator.clipboard.writeText(window.location.href)
+        setShareLinkCopied(true)
+        setTimeout(()=>{
+            setShareLinkCopied(false)
+        }, 2000)
+       }}>
+                <FiShare2 className='text-lg text-slate-500'>
+
+                </FiShare2>
+        </div>
+        {shareLinkCopied && (
+            <p className='fixed top-[8%] right-[10%] font-semibold border-2
+            border-color-gray-400 rounded-md bg-white z-10 p-2'>Link Copied</p>
+        )}
     </main>
   )
 }
